7b64aa498d035b8ea09d01ee8d54b7e4ccfbb615
Karan Sharma <sharma.karan27oct@gmail.com>
2024-05-20 11:25:00 +0530

    added port

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ app.get('/form-submission', async (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
 });
-app.listen( 5000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen( PORT, () => {
     console.log('Server is running on port 5000');
 });
